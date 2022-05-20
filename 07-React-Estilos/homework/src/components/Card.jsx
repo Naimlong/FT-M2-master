78571d341fa580cd015778b4f790b06f39c9af96
Naim Longinotti <naimlong979@hotmail.com>
2022-05-19 23:42:25 -0300

    07

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import s from "./Card.module.css";
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+   <div className={s.card}>
+      { <button onClick={props.onClose} className={`${s.btn}`}>X</button> }
+      <h3>{props.name}</h3>
+      <div className={s.middlevDiv}>
+        <p>Min</p>
+        <p>{props.min}</p>
+        <p>Max</p>
+        <p>{props.max}</p>
+      </div>
+      <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="" />
+  
+    </div>)
 };
